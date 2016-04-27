2ce39e38415bdcb58ae904ce8bcc455b2f88f33a
KAIRA N. JOHNSON <KJOHNSON1913@GMAIL.COM>
2016-04-26 23:44:18 -0400

    angular routers added

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,24 @@
 (function(){
 angular
 .module("Gentrification", [
-
+"ui.router"
 ])
+
+.config(Router);
+
+  Router.$inject = ["$stateProvider", "$locationProvider",
+"$urlRouterProvider"];
+  function Router($stateProvider, $locationProvider, $urlRouterProvider){
+    $locationProvider.html5Mode(true);
+    $stateProvider
+    .state("home", {
+      url:      "/home",
+      template: "<h1> homepage </h1>"
+    })
+    .state("archive", {
+      url:      "/archive",
+      template: "<h1>archive page</h1>"
+    });
+    $urlRouterProvider.otherwise("/");
+  }
 })();
